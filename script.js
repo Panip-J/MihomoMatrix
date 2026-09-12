a75636477bacbb2962b1766110f705cbb63cc576
Panip-J <Panip-J@users.noreply.github.com>
2026-09-12 14:30:13 +0800

    feat: switch all proxy group icons to github emoji png format

--- script.js
+++ script.js
@@ -124,34 +124,34 @@ function main(config, profileName) {
     'MATCH,SLIP'
   ];
   const countryDefs = [
-    { name: 'HKG', regex: /(?:香港|HK(?![a-zA-Z])|HongKong|Hong Kong)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/hk.svg' },
-    { name: 'MAC', regex: /(?:澳门|MO(?![a-zA-Z])|Macau|Macao)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/mo.svg' },
-    { name: 'TWN', regex: /(?:台湾|TW(?![a-zA-Z])|Taiwan)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/tw.svg' },
-    { name: 'JPN', regex: /(?:日本|JP(?![a-zA-Z])|Japan)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/jp.svg' },
-    { name: 'KOR', regex: /(?:韩国|KR(?![a-zA-Z])|Korea)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/kr.svg' },
-    { name: 'SGP', regex: /(?:新加坡|SG(?![a-zA-Z])|Singapore)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/sg.svg' },
-    { name: 'MYS', regex: /(?:马来|MY(?![a-zA-Z])|Malaysia)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/my.svg' },
-    { name: 'THA', regex: /(?:泰国|TH(?![a-zA-Z])|Thailand)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/th.svg' },
-    { name: 'VNM', regex: /(?:越南|VN(?![a-zA-Z])|Vietnam)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/vn.svg' },
-    { name: 'IDN', regex: /(?:印尼|印度尼西亚|ID(?![a-zA-Z])|Indonesia)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/id.svg' },
-    { name: 'IND', regex: /(?:(?<!印)印度(?!尼西亚)|IN(?![a-zA-Z])|India)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/in.svg' },
-    { name: 'ARE', regex: /(?:阿拉伯联合酋长国|阿联酋|迪拜|阿布扎比|AE(?![a-zA-Z])|UAE|United Arab Emirates)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/ae.svg' },
-    { name: 'USA', regex: /(?:美国|US(?![a-zA-Z])|UnitedStates|United States)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/us.svg' },
-    { name: 'CAN', regex: /(?:加拿大|CA(?![a-zA-Z])|Canada)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/ca.svg' },
-    { name: 'MEX', regex: /(?:墨西哥|MX(?![a-zA-Z])|Mexico)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/mx.svg' },
-    { name: 'BRA', regex: /(?:巴西|BR(?![a-zA-Z])|Brazil)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/br.svg' },
-    { name: 'UKR', regex: /(?:乌克兰|UA(?![a-zA-Z])|Ukraine)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/ua.svg' },
-    { name: 'ROU', regex: /(?:罗马尼亚|RO(?![a-zA-Z])|Romania)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/ro.svg' },
-    { name: 'LTU', regex: /(?:立陶宛|LT(?![a-zA-Z])|Lithuania)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/lt.svg' },
-    { name: 'ITA', regex: /(?:意大利|IT(?![a-zA-Z])|Italy)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/it.svg' },
-    { name: 'TUR', regex: /(?:土耳其|TR(?![a-zA-Z])|Turkey)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/tr.svg' },
-    { name: 'GBR', regex: /(?:英国|UK(?![a-zA-Z])|GreatBritain|UnitedKingdom)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/gb.svg' },
-    { name: 'DEU', regex: /(?:德国|DE(?![a-zA-Z])|Germany)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/de.svg' },
-    { name: 'FRA', regex: /(?:法国|FR(?![a-zA-Z])|France)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/fr.svg' },
-    { name: 'NLD', regex: /(?:荷兰|NL(?![a-zA-Z])|Netherlands)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/nl.svg' },
-    { name: 'SWE', regex: /(?:瑞典|SE(?![a-zA-Z])|Sweden)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/se.svg' },
-    { name: 'AUS', regex: /(?:澳大利亚|澳洲|AU(?![a-zA-Z])|Australia)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/au.svg' },
-    { name: 'NGA', regex: /(?:尼日利亚|NG(?![a-zA-Z])|Nigeria)/i, flag: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/ng.svg' }
+    { name: 'HKG', regex: /(?:香港|HK(?![a-zA-Z])|HongKong|Hong Kong)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1ed-1f1f0.png?v8' },
+    { name: 'MAC', regex: /(?:澳门|MO(?![a-zA-Z])|Macau|Macao)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f2-1f1f4.png?v8' },
+    { name: 'TWN', regex: /(?:台湾|TW(?![a-zA-Z])|Taiwan)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f9-1f1fc.png?v8' },
+    { name: 'JPN', regex: /(?:日本|JP(?![a-zA-Z])|Japan)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1ef-1f1f5.png?v8' },
+    { name: 'KOR', regex: /(?:韩国|KR(?![a-zA-Z])|Korea)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f0-1f1f7.png?v8' },
+    { name: 'SGP', regex: /(?:新加坡|SG(?![a-zA-Z])|Singapore)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f8-1f1ec.png?v8' },
+    { name: 'MYS', regex: /(?:马来|MY(?![a-zA-Z])|Malaysia)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f2-1f1fe.png?v8' },
+    { name: 'THA', regex: /(?:泰国|TH(?![a-zA-Z])|Thailand)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f9-1f1ed.png?v8' },
+    { name: 'VNM', regex: /(?:越南|VN(?![a-zA-Z])|Vietnam)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1fb-1f1f3.png?v8' },
+    { name: 'IDN', regex: /(?:印尼|印度尼西亚|ID(?![a-zA-Z])|Indonesia)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1ee-1f1e9.png?v8' },
+    { name: 'IND', regex: /(?:(?<!印)印度(?!尼西亚)|IN(?![a-zA-Z])|India)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1ee-1f1f3.png?v8' },
+    { name: 'ARE', regex: /(?:阿拉伯联合酋长国|阿联酋|迪拜|阿布扎比|AE(?![a-zA-Z])|UAE|United Arab Emirates)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1e6-1f1ea.png?v8' },
+    { name: 'USA', regex: /(?:美国|US(?![a-zA-Z])|UnitedStates|United States)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1fa-1f1f8.png?v8' },
+    { name: 'CAN', regex: /(?:加拿大|CA(?![a-zA-Z])|Canada)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1e8-1f1e6.png?v8' },
+    { name: 'MEX', regex: /(?:墨西哥|MX(?![a-zA-Z])|Mexico)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f2-1f1fd.png?v8' },
+    { name: 'BRA', regex: /(?:巴西|BR(?![a-zA-Z])|Brazil)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1e7-1f1f7.png?v8' },
+    { name: 'UKR', regex: /(?:乌克兰|UA(?![a-zA-Z])|Ukraine)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1fa-1f1e6.png?v8' },
+    { name: 'ROU', regex: /(?:罗马尼亚|RO(?![a-zA-Z])|Romania)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f7-1f1f4.png?v8' },
+    { name: 'LTU', regex: /(?:立陶宛|LT(?![a-zA-Z])|Lithuania)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f1-1f1f9.png?v8' },
+    { name: 'ITA', regex: /(?:意大利|IT(?![a-zA-Z])|Italy)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1ee-1f1f9.png?v8' },
+    { name: 'TUR', regex: /(?:土耳其|TR(?![a-zA-Z])|Turkey)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f9-1f1f7.png?v8' },
+    { name: 'GBR', regex: /(?:英国|UK(?![a-zA-Z])|GreatBritain|UnitedKingdom)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1ec-1f1e7.png?v8' },
+    { name: 'DEU', regex: /(?:德国|DE(?![a-zA-Z])|Germany)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1e9-1f1ea.png?v8' },
+    { name: 'FRA', regex: /(?:法国|FR(?![a-zA-Z])|France)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1eb-1f1f7.png?v8' },
+    { name: 'NLD', regex: /(?:荷兰|NL(?![a-zA-Z])|Netherlands)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f3-1f1f1.png?v8' },
+    { name: 'SWE', regex: /(?:瑞典|SE(?![a-zA-Z])|Sweden)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f8-1f1ea.png?v8' },
+    { name: 'AUS', regex: /(?:澳大利亚|澳洲|AU(?![a-zA-Z])|Australia)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1e6-1f1fa.png?v8' },
+    { name: 'NGA', regex: /(?:尼日利亚|NG(?![a-zA-Z])|Nigeria)/i, flag: 'https://github.githubassets.com/images/icons/emoji/unicode/1f1f3-1f1ec.png?v8' }
   ];
   const countryGroups = [];
   const allMatchedNames = new Set();
@@ -170,7 +170,7 @@ function main(config, profileName) {
     name: 'OTH',
     type: 'select',
     proxies: othMatched.length > 0 ? othMatched : ['DIRECT'],
-    icon: 'https://cdn.jsdelivr.net/gh/hatscripts/circle-flags@gh-pages/flags/xx.svg'
+    icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f3f4-2620.png?v8'
   });
   const defaultProxies = [
     'DIRECT',
@@ -186,43 +186,43 @@ function main(config, profileName) {
       name: 'DEFAULT',
       type: 'select',
       proxies: defaultProxies,
-      icon: 'https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Proxy.png'
+      icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f680.png?v8'
     },
     {
       name: 'ALL',
       type: 'select',
       proxies: proxyNames.length > 0 ? proxyNames : ['DIRECT'],
-      icon: 'https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Global.png'
+      icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f30f.png?v8'
     },
     {
       name: 'STORE',
       type: 'select',
       proxies: ['HKG', 'DEFAULT', 'DIRECT'],
-      icon: 'https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Steam.png'
+      icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f3ae.png?v8'
     },
     {
       name: 'WIKI',
       type: 'select',
       proxies: ['HKG', 'DEFAULT', 'DIRECT'],
-      icon: 'https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Puzzle.png'
+      icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f4da.png?v8'
     },
     {
       name: 'PIKPAK',
       type: 'select',
       proxies: ['HKG', 'DEFAULT', 'DIRECT'],
-      icon: 'https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/iCloud.png'
+      icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f5c3.png?v8'
     },
     {
       name: 'SRGB',
       type: 'select',
       proxies: ['USA', 'DEFAULT', 'DIRECT'],
-      icon: 'https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Rainbow.png'
+      icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f4a1.png?v8'
     },
     {
       name: 'SLIP',
       type: 'select',
       proxies: ['DEFAULT', 'DIRECT'],
-      icon: 'https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Final.png'
+      icon: 'https://github.githubassets.com/images/icons/emoji/unicode/1f41f.png?v8'
     }
   ];
   config['proxy-groups'] = [
